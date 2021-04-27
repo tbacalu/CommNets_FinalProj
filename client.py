ff57e83cc1ff6da8c3901489636cdc1bcba1b7f4
Ubuntu <ubuntu@ip-172-31-16-142.us-east-2.compute.internal>
2021-04-27 22:42:50 +0000

    updated client

--- client.py
+++ client.py
@@ -2,21 +2,26 @@ import requests
 import sys
 import json
 
+
 def getMessages(user):
-    params = {"user":user}
-    r = requests.get("http://ec2-3-139-54-119.us-east-2.compute.amazonaws.com", params)
+    params = {"user": user}
+    r = requests.get(
+        "http://ec2-3-139-54-119.us-east-2.compute.amazonaws.com", params)
     print(f"\nMessages for {user}:")
     for message in r.json()['messages']:
         print(f"({message['sender']}) {message['value']}")
     print("")
 
+
 def send(to_usr, msg):
-    print(f"Sending {msg} to {to_user}")
-    data = {"sender": user, "receiver": to_user, "message": msg}
-    r = requests.post("http://ec2-3-139-54-119.us-east-2.compute.amazonaws.com", data)
+    print(f"Sending {msg} to {to_usr}")
+    data = {"sender": user, "receiver": to_usr, "message": msg}
+    r = requests.post(
+        "http://ec2-3-139-54-119.us-east-2.compute.amazonaws.com", data)
+
 
 user = sys.argv[1]
-getMessages(user)
+#getMessages(user)
 
 while True:
     print("Choose a command:\n1) refresh\n2) send:<to_usr>:<msg>\n3) quit")
@@ -27,7 +32,7 @@ while True:
         send(resp[1], resp[2])
 
     elif resp.lower() == "refresh":
-	getMessages(user)
-	
+        getMessages(user)
+
     elif resp.lower() == "quit":
-	break
+        break
